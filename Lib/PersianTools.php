8f1e1f8ee018dcfc909e48eba8aa6e2840122979
intuxicated <hidensoft@gmail.com>
2013-03-30 05:06:27 +0000

    add service

--- Lib/PersianTools.php
+++ Lib/PersianTools.php
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author Mohammad niknam 
+ * @author Mohammad niknam
  * @author Vahid Sohrablou (IranPHP.org)
  */
 
